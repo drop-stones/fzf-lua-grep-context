7952a3047d27cc8a270310412f21d98bfea7da5a
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-04-21 21:48:59 +0900

    feat(init): Create entry point

---
+++ lua/fzf-lua-grep-context/init.lua
@@ -0,0 +1,11 @@
+-- Main entry point for fzf-lua-grep-context plugin
+
+local M = {}
+
+---Setup the plugin with the given user options
+---@param opts? FzfLuaGrepContextOptions
+function M.setup(opts)
+  opts = opts or {}
+end
+
+return M
